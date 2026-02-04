cd6d323764a089819706ae4a41e38c694e0c299c
Yash Ejjagiri <ejjagiri.yaswant24@gmail.com>
2026-02-04 17:31:23 -0600

    Adding job scripts

--- jobs/swin_lsst_moco_lsst.sh
+++ jobs/swin_lsst_moco_lsst.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH -J moco5_30k_test
-#SBATCH --output=/projects/bfhm/yse2/logs/training_logs/moco5_30k_test_out.%j.out
-#SBATCH --error=/projects/bfhm/yse2/logs/training_logs/moco5_30k_test_err.%j.err
+#SBATCH -J only_moco5_30k_rubin_test
+#SBATCH --output=/projects/bfhm/yse2/logs/training_logs/only_moco5_30k_rubin_test_out.%j.out
+#SBATCH --error=/projects/bfhm/yse2/logs/training_logs/only_moco5_30k_rubin_test_err.%j.err
 #SBATCH --account=bdsp-delta-gpu
-#SBATCH --partition=gpuA40x4
+#SBATCH --partition=gpuA100x4
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4         # Using 4 GPUs on the node
 #SBATCH --mem=128G    
 #SBATCH --ntasks-per-node=1        # one process per GPU for distributed training
 #SBATCH --cpus-per-task=64       # 64 CPUs divided by GPUs for data loading
-#SBATCH --reservation=RH8
 #SBATCH --constraint="work"
-#SBATCH --time=02:00:00
+#SBATCH --time=01:30:00
 #SBATCH --mail-user=yse2@illinois.edu
 #SBATCH --mail-type=ALL
 
@@ -32,21 +31,20 @@ echo "Running on GPUs:"
 nvidia-smi --query-gpu=gpu_name,gpu_bus_id,memory.total,memory.used --format=csv
 echo "Defining training parameters..."
 # locations of config and data files
-CFG_FILE="/u/yse2/deepdisc/configs/solo/swin_lsst_job_moco.py"
+CFG_FILE="/u/yse2/deepdisc/configs/solo/swin_lsst_job_moco_lsst.py"
 TRAIN_FILE="/u/yse2/lsst_data/annotations_lvl5/train_30k.json" 
 EVAL_FILE="/u/yse2/lsst_data/annotations_lvl5/val_4k.json"  
 
 # RUN_NAME="moco5_30k_4h200_bs256_ep50"
 # RUN_NAME="moco5_30k_4h200_bs256_ep50_frz"
-# RUN_NAME="moco5_30k_4h200_bs256_ep50_frz_4moco"
-RUN_NAME="moco5_30k_test"
+RUN_NAME="only_moco5_30k_rubin_test"
 OUTPUT_DIR="./lsst_runs/${RUN_NAME}"
 
 echo "Launching DeepDISC training job..."
 
-# python run_model_job_moco.py --cfgfile /u/yse2/deepdisc/configs/solo/swin_lsst_job_moco.py --train-metadata /u/yse2/lsst_data/annotations_lvl5/train_30k.json --eval-metadata /u/yse2/lsst_data/annotations_lvl5/val_4k.json --num-gpus 2 --run-name moco5_30k_test --output-dir ./lsst_runs/moco5_30k_test
+# python run_model_job_moco_lsst.py --cfgfile /u/yse2/deepdisc/configs/solo/swin_lsst_job_moco_lsst.py --train-metadata /u/yse2/lsst_data/annotations_lvl5/train_30k.json --eval-metadata /u/yse2/lsst_data/annotations_lvl5/val_4k.json --num-gpus 4 --run-name moco5_30k_rubin_test --output-dir ./lsst_runs/moco5_30k_rubin_test
 
-python run_model_job_moco.py \
+python run_model_job_moco_lsst.py \
     --cfgfile ${CFG_FILE} \
     --train-metadata ${TRAIN_FILE} \
     --eval-metadata ${EVAL_FILE} \
